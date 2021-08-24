8ad3e0cf4a06a13b06e718d755f497eabef9b269
AKSHAY SANZE <60998061+akshaysanze006@users.noreply.github.com>
2021-08-24 12:38:32 +0530

    Create process comuncaton.java

---
+++ process comuncaton.java
@@ -0,0 +1,37 @@
+import java.io.IOException; 
+import java.io.PipedInputStream; 
+import java.io.PipedOutputStream 
+public class PipeExample 
+{
+20 
+Department of Computer Science and Engineering Vedavyasa Institute of Technology 
+public static void main(string[] args) throws IOException { 
+final PipedOutputStream Output=new PipedOutputStream(); final PipedInputStream Input=new Thread(new Runnable() { 
+ public void run() 
+ { 
+ try 
+ { Output.write(“Hello world!”.getBytes()); 
+ } 
+} 
+catch(IOException e) 
+{ 
+} 
+}  
+}); 
+Thread thread2=new Thread(new Runnable()  
+{ 
+Public void run() 
+{ 
+try 
+{ 
+Int data=Input.read(); 
+While(data!=-1){ 
+System.out.println((char)data); 
+}} 
+Catch(IOException e){ 
+}} 
+}); 
+tread1.start(); 
+thread2.start(); 
+} 
+} 
